0997a1720d86b983398c15d7c35b8eeb084ebf88
benjamin852 <weinbergben33@gmail.com>
2023-11-14 14:00:01 +0000

    added completed code

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -1,13 +1,9 @@
 import { MoralisNextApi } from '@moralisweb3/next';
 
+const MORALIS_API_KEY = 'JSubmrWUErdQR3QUTSztwNzpfO84QhKL6b5IFo5u9FzH2VsvBt4LEoQpsOqa56ZU';
 const NEXTAUTH_URL = 'http://localhost:3000';
-
-if (process.env.MORALIS_API_KEY === undefined || typeof process.env.MORALIS_API_KEY !== 'string') {
-  throw new Error('process.env.apiKey is missing or not a string');
-}
-
 export default MoralisNextApi({
-  apiKey: process.env.MORALIS_API_KEY,
+  apiKey: 'JSubmrWUErdQR3QUTSztwNzpfO84QhKL6b5IFo5u9FzH2VsvBt4LEoQpsOqa56ZU',
   authentication: {
     domain: NEXTAUTH_URL ? new URL(NEXTAUTH_URL).host : '',
     uri: 'http://localhost:3000',
